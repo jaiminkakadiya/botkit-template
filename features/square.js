35c86cfb4d1dcb292506a4e4aff87ff8656d8146
jaiminkakadiya <55538756+jaiminkakadiya@users.noreply.github.com>
2020-05-20 17:51:28 +0100

    Update square.js

--- features/square.js
+++ features/square.js
@@ -12,7 +12,7 @@ module.exports = function( controller ) {
     );
     convo.say( 'Square of {{vars.stated_number}} is ');
     var a = 23;
-    convo.say(a);
+    convo.say(to.String(a));
 
     controller.addDialog( convo );
 
