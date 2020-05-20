d7652da1b624590e8ccffbd655c6cb9902b93a70
jaiminkakadiya <55538756+jaiminkakadiya@users.noreply.github.com>
2020-05-20 16:46:08 +0100

    Update square.js

--- features/square.js
+++ features/square.js
@@ -10,7 +10,9 @@ module.exports = function( controller ) {
         async( answer, convo, bot ) => {},
         'stated_number'
     );
-    convo.say( `Square of {{ vars.stated_number }} : ${vars.stated_number*vars.stated_number }` );
+    convo.say( 'Square of {{ vars.stated_number }} ',
+        var a = vars.stated_number*vars.stated_number,
+        '{{a}}' );
 
     controller.addDialog( convo );
 
